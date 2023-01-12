bcc5c07eaa28b3b018b0fc2fc67f0706ca6a438f
Pedro Roldan <pedroroldan.devoteam@edp.com>
2023-01-12 00:08:26 +0000

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fishlife/main.dart';
+import 'package:fishlife/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
